e99d2e0d92b5ad1650e4554a33b15333291c94c5
Alexey Alekhin <laughedelic@gmail.com>
2015-08-30 05:16:17 +0200

    Added transfer manager with multipart download

--- src/main/scala/bio4j-dist-lite.scala
+++ src/main/scala/bio4j-dist-lite.scala
@@ -3,9 +3,10 @@ package ohnosequences.statika
 import ohnosequences.statika._, bundles._, instructions._
 import ohnosequences.awstools.s3._
 
-import com.amazonaws.auth._ //, profile._
+import com.amazonaws.auth._, profile._
 import com.amazonaws.services.s3._
 import com.amazonaws.services.s3.model._
+import com.amazonaws.services.s3.transfer._
 
 import java.io.File
 import sys.process._
@@ -24,27 +25,23 @@ case class MountSSD(device: String) extends Bundle() {
 abstract class Bio4jDist(name: String) extends Bundle(MountSSD("xvdb")) {
   val region = "eu-west-1"
 
-  val dbDistObject = ObjectAddress(s"${region}.releases.bio4j.com", s"2014_12_03/${name}")
-  val dbLocation = new File(s"/media/ephemeral0/${name}")
+  val dbDistObject: ObjectAddress
+  // = ObjectAddress(s"${region}.releases.bio4j.com", s"2014_12_03/${name}")
+  val dbLocation = new File(s"/media/ephemeral0/")
 
   def instructions: AnyInstructions = {
 
     LazyTry {
-      // val s3 = S3.create(new InstanceProfileCredentialsProvider()) // we rely on instance role credentials
-      // val s3 = S3.create(new ProfileCredentialsProvider("default")) // we rely on instance role credentials
-      // val s3 = new AmazonS3Client(new ProfileCredentialsProvider("default")) // we rely on instance role credentials
-      val s3 = new AmazonS3Client(new InstanceProfileCredentialsProvider()) // we rely on instance role credentials
-      val request = new GetObjectRequest(dbDistObject.bucket, dbDistObject.key, true)
+      val transferManager = new TransferManager(new InstanceProfileCredentialsProvider())
+      // val transferManager = new TransferManager(new ProfileCredentialsProvider("default"))
 
-      // if (!dbLocation.exists) dbLocation.mkdirs
+      if (!dbLocation.exists) dbLocation.mkdirs
 
       println(s"Dowloading from: ${dbDistObject}")
       println(s"File: ${dbLocation.getAbsolutePath}")
 
-      // val loader = s3.createLoadingManager
-      // val download = loader.transferManager.download(request, dbLocation)
-      // loader.transferWaiter(download)
-      s3.getObject(request, dbLocation)
+      val transfer = transferManager.downloadDirectory(dbDistObject.bucket, dbDistObject.key, dbLocation)
+      transfer.waitForCompletion
     } -&-
     say(s"Distribution ${bundleName} was dowloaded to ${dbLocation}")
 
@@ -57,5 +54,5 @@ abstract class Bio4jDist(name: String) extends Bundle(MountSSD("xvdb")) {
 
 case object bio4jTaxonomy extends Bio4jDist("taxonomy") {
 
-  override val dbDistObject = ObjectAddress(s"${region}.resources.ohnosequences.com", "16s/bio4j")
+  val dbDistObject = ObjectAddress("resources.ohnosequences.com", "16s/bio4j")
 }
