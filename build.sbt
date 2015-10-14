a0f9e7ce86f22ac1ff0a3661e546c48e3ce7ab2d
Alexey Alekhin <laughedelic@gmail.com>
2015-10-14 21:01:07 +0200

    Updated the code; moved things from MG7

--- build.sbt
+++ build.sbt
@@ -12,6 +12,18 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "ohnosequences" %% "statika" % "2.0.0-new-instructions-SNAPSHOT",
-  "ohnosequences" %% "aws-scala-tools" % "0.13.2"
+  "ohnosequences" %% "statika"         %  "2.0.0-M4",
+  "ohnosequences" %% "aws-scala-tools" %  "0.14.0",
+  "bio4j"         %  "bio4j"           %  "0.12.0-RC3",
+  "bio4j"         %  "bio4j-titan"     %  "0.4.0-RC2"
+)
+
+dependencyOverrides ++= Set(
+  "com.fasterxml.jackson.core" % "jackson-core"        % "2.3.2",
+  "com.fasterxml.jackson.core" % "jackson-databind"    % "2.3.2",
+  "com.fasterxml.jackson.core" % "jackson-annotations" % "2.3.2",
+  "commons-logging"            % "commons-logging"     % "1.1.3",
+  "commons-codec"              % "commons-codec"       % "1.7",
+  "org.apache.httpcomponents"  % "httpclient"          % "4.5",
+  "org.slf4j"                  % "slf4j-api"           % "1.7.7"
 )
---
+++ src/main/scala/bio4j-dist.scala
@@ -0,0 +1,65 @@
+package ohnosequencesBundles.statika
+
+import ohnosequences.statika._, bundles._, instructions._
+import ohnosequences.awstools._, s3._, regions._
+
+import com.amazonaws.auth._
+import com.amazonaws.services.s3.transfer._
+
+import java.io.File
+
+import com.thinkaurelius.titan.core._
+import com.bio4j.titan.model.ncbiTaxonomy._
+import com.bio4j.titan.util.DefaultTitanGraph
+import org.apache.commons.configuration.Configuration
+import org.apache.commons.configuration.BaseConfiguration
+
+
+trait AnyBio4jDist extends Bundle() {
+
+  val s3folder: S3Folder
+  val configuration: Configuration
+
+  private lazy val destination: File = new File(".")
+  lazy val dbLocation: File = new File(destination, s3folder.key)
+
+  def instructions: AnyInstructions = LazyTry {
+    println(s"""Dowloading
+      |from: ${s3folder.url}
+      |to: ${destination.getCanonicalPath}
+      |""".stripMargin)
+
+    val transferManager = new TransferManager(new InstanceProfileCredentialsProvider())
+    transferManager.downloadDirectory(s3folder.bucket, s3folder.key, destination).waitForCompletion
+  }
+}
+
+abstract class Bio4jDist(
+  val region: Region,
+  val version: String,
+  val name: String
+) extends AnyBio4jDist { dist =>
+
+  lazy val s3folder: S3Folder = S3Folder(
+    s"${dist.region}.releases.bio4j.com",
+    s"${dist.version}/${dist.name}"
+  )
+
+  lazy val configuration: Configuration = {
+    val base = new BaseConfiguration()
+    base.setProperty("storage.directory", dbLocation.getCanonicalPath)
+    base.setProperty("storage.backend", "berkeleyje")
+    base.setProperty("storage.batch-loading", "false")
+    base.setProperty("storage.transactions", "true")
+    base.setProperty("query.fast-property", "false")
+    base.setProperty("schema.default", "none")
+    base
+  }
+}
+
+  // // the graph; its only (direct) use is for indexes
+  // // FIXME: this works but still with errors, should be fixed (something about transactions)
+  // lazy val graph: TitanNCBITaxonomyGraph =
+  //   new TitanNCBITaxonomyGraph(
+  //     new DefaultTitanGraph(TitanFactory.open(configuration))
+  //   )
